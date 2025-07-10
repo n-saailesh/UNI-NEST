cd94b8a068392acb0f83e45f2a7282b49222eb29
n-saailesh <saailesh213@gmail.com>
2025-07-10 00:57:01 -0400

    12:56

--- script.py
+++ script.py
@@ -2,13 +2,16 @@
 
 from flask import Flask, request, render_template
 
+#Setting up the Flask framwork and tempalte folder for the pathway of all the HTML files
 app = Flask(__name__, template_folder=r'C:\Users\MyPC\OneDrive\VS Code Projects\uninest')
 
+#When the user goes to the preferences page, it will render the preference.html amd ask the user to fill the quick form (GET by default)
 @app.route('/preferences.html')
 def preferences():
     return render_template('preferences.html')
 
-@app.route('/a', methods = ['GET','POST'])
+#When the user submits the form, it will retrieve the data from the form and send it to the sever (POST)
+@app.route('/a', methods = ['POST'])
 def submit():
 
     if request.method == "POST":
@@ -26,7 +29,6 @@ def submit():
 
         return render_template('preferences.html', string=string)
 
-        print(string)
     return render_template('preferences.html')
 
 
